1381352cf3b242e858139125efe31ad66c81eff7
Adeeb Siddiqui <adeeb.bd@gmail.com>
2020-06-18 18:58:42 -0500

    Created consumer for json-topic

--- src/main/java/com/ad/app/service/KafkaConsumer.java
+++ src/main/java/com/ad/app/service/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.ad.app.service;
 
+import com.ad.app.model.Laptop;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,10 @@ public class KafkaConsumer {
     @KafkaListener(topics = "simple-topic", groupId = "simple-group")
     public void consume(String message) {
         System.out.println("Consumed message: " + message);
+    }
+
+    @KafkaListener(topics = "json-topic", groupId = "json-group", containerFactory = "laptopKafkaListenerFactory")
+    public void consume(Laptop laptop) {
+        System.out.println("Consumed message: " + laptop);
     }
 }
